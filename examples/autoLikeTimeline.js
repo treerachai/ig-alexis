2e51d08c50a460b9281ab5d68fdf22ef43bfc9f5
AlfathDirk <alfathdirk@gmail.com>
2018-06-29 13:13:20 +0700

    add example for autolike tl

---
+++ examples/autoLikeTimeline.js
@@ -0,0 +1,33 @@
+const Ig = require('../main');
+let ig = new Ig();
+
+const TIMEOUT = 6000; // 6 seconds;
+
+const username = 'your username';
+const password = 'your password';
+
+async function doLike() {
+  try {
+    await ig.loginByPassword({ username, password });
+    let medias = await ig.getTimeLineMedia();
+    medias.map(async ({ node }) => {
+        let now = new Date().getTime();
+        let fourHours = 3600 * 4 * 1000;
+        let mediaTimePost = node.taken_at_timestamp * 1000;
+        if(mediaTimePost >= now - fourHours) {
+            if(!node.viewer_has_liked) {
+                let x = await ig.likeMedia(node.id);
+                console.log(x);
+                return x;
+            }
+        }
+    })
+  } catch (error) {
+    console.error(error);
+  }
+  setTimeout(doLike, TIMEOUT)
+}
+
+doLike();
+
+// module.exports = doLike;
