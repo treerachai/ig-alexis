51950d66714626e3eae559ce1aa7c3db8c75f8ba
AlfathDirk <alfathdirk@gmail.com>
2018-10-30 15:18:03 +0700

    add upload photo

--- examples/autoLikeTimeline.js
+++ examples/autoLikeTimeline.js
@@ -3,14 +3,15 @@ let ig = new Ig();
 
 const TIMEOUT = 6000; // 6 seconds;
 
-const username = 'your username';
-const password = 'your password';
+// const username = 'username';
+// const password = 'password';
 
 async function doLike() {
   try {
     await ig.loginByPassword({ username, password });
     let medias = await ig.getTimeLineMedia();
     medias.map(async ({ node }) => {
+      console.log(node);
         let now = new Date().getTime();
         let fourHours = 3600 * 4 * 1000;
         let mediaTimePost = node.taken_at_timestamp * 1000;
@@ -25,7 +26,7 @@ async function doLike() {
   } catch (error) {
     console.error(error);
   }
-  setTimeout(doLike, TIMEOUT)
+  // setTimeout(doLike, TIMEOUT)
 }
 
 doLike();
